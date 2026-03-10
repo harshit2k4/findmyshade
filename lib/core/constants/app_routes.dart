3e2559f817c5470fc5b32e6cb59a51957090f8eb
Harshit <84241825+harshit2k4@users.noreply.github.com>
2026-03-11 02:03:56 +0530

    feat(navigation): define routing screens

---
+++ lib/core/constants/app_routes.dart
@@ -0,0 +1,8 @@
+class AppRoutes {
+  AppRoutes._();
+
+  static const home = '/';
+  static const skinTone = '/skin-tone';
+  static const brands = '/brands';
+  static const results = '/results';
+}
